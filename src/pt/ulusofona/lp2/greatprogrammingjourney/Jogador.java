5d1ecd0b4a45200aef36debeb27c74764ba5e301
Rafael Magalhaes <a22407372@alunos.ulht.pt>
2025-10-28 12:17:49 +0000

    Adicionei getters á classe Jogador.java .

--- src/pt/ulusofona/lp2/greatprogrammingjourney/Jogador.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/Jogador.java
@@ -4,16 +4,30 @@ import java.util.ArrayList;
 
 public class Jogador {
     int id;
-    int corAvatar;
+    String corAvatar;
     String nome;
     ArrayList<String> linguagensFavoritas;
     String posicaoAtual;
     Boolean estaEmJogo;
 
-    public Jogador(int id, int corAvatar) {
+    public Jogador(int id, String corAvatar) {
         this.id = id;
         this.corAvatar = corAvatar;
     }
 
+    int getId(){
+        return id;
+    }
 
+    String getCorAvatar(){
+        return corAvatar;
+    }
+
+    String getNome(){
+        return nome;
+    }
+
+    String getPosicaoAtual(){
+        return posicaoAtual;
+    }
 }
