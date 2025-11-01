bdd7ce514b165f185eec2dc0bf259a55d97645a3
Diogo Alves <a22407390@alunos.ulht.pt>
2025-11-01 23:06:39 +0000

    .

--- src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
@@ -16,18 +16,18 @@ public class GameManager {
 
     public boolean createInitialBoard(String[][] playerInfo, int worldSize) {
         Tabuleiro tabuleiro = new Tabuleiro(playerInfo, worldSize);
-        for (int i = 0; i < playerInfo.length; i++) {
-            int id = Integer.parseInt(playerInfo[i][0]);
-            String nome = playerInfo[i][1];
-            String linguagens = playerInfo[i][2];
-            String cor = playerInfo[i][3];
-
-            players[i] = new Jogador(id, nome, linguagens, cor);
-        }
         if ((worldSize >= playerInfo.length * 2)
                 && tabuleiro.verificarCores(playerInfo)
                 && tabuleiro.verificarNomesValidos(playerInfo)
                 && tabuleiro.verificarIdsValidosERepetidos(playerInfo)) {
+            for (int i = 0; i < playerInfo.length; i++) {
+                int id = Integer.parseInt(playerInfo[i][0]);
+                String nome = playerInfo[i][1];
+                String linguagens = playerInfo[i][2];
+                String cor = playerInfo[i][3];
+
+                players[i] = new Jogador(id, nome, linguagens, cor);
+            }
             tamanhoFinalTabuleiro = worldSize;
             return true;
         }
